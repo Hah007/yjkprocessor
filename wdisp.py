94dad3e64d139224984bcb310fbb151b515b0588
hah007 <hah007@126.com>
2021-01-27 19:31:52 +0800

    b1-1

--- wdisp.py
+++ wdisp.py
@@ -70,8 +70,7 @@ def data_into_xls(xls_filename,data_filename):
     
 def get_X_Equivalent_Disp_info(filename,pattern_mode):
     X_E_Dispangle = []
-    X_W_Dispangle_f = []
-    Y_E_Dispangle = []
+    X_E_Dispangle_f = []  
     floor=[]
     info = get_block_info(pattern_mode)
     # print(info)
@@ -90,7 +89,7 @@ def get_X_Equivalent_Disp_info(filename,pattern_mode):
             if  i%2==1:
                 stringtemp =tt[49:55]
                 nostringtemp = ''.join(stringtemp.split())
-                X_W_Dispangle_f.append(nostringtemp)
+                X_E_Dispangle_f.append(nostringtemp)
                 # print(stringtemp)
                 nostringtemp=convert_to_float(stringtemp)
                 X_E_Dispangle.append(nostringtemp)
@@ -99,30 +98,20 @@ def get_X_Equivalent_Disp_info(filename,pattern_mode):
                 nostringtemp = ''.join(stringtemp1.split())
                 nostringtemp=convert_to_float(nostringtemp)
                 floor.append(nostringtemp)       
-
-    # print("**********")
-    # print(floor)
-    # print("**********")      
-    # print(X_E_Dispangle)       
-    # print("**********")
     pd1=pd.DataFrame(floor,columns=['楼层'])
     pd2=pd.DataFrame(X_E_Dispangle,columns=['位移角'])
-    pd21=pd.DataFrame(X_W_Dispangle_f,columns=['分数位移角'])
+    pd21=pd.DataFrame(X_E_Dispangle_f,columns=['分数位移角'])
     # pd.to_numeric(pd1, errors='coerce')
     # pd.to_numeric(pd2, errors='coerce')
     pd3=pd.concat([pd1,pd2,pd21],axis=1)
     print(pd3)
     print('***'*20)
     print(pd3.dtypes)
-
-    # pd4['B'].plot.barh(stacked=True)
-    # pd4.plot.barh(stacked=True)
     pd3['位移角'].plot.line(subplots=True)
     plt.show()
     time.sleep(1)
-    # pd3.plot.bar(stacked=True)
 
-    filepath='X方向地震作用下的楼层最大位移.xls'
+    filepath="%s.xls" %filename
     writer = pd.ExcelWriter(filepath)
     pd3.to_excel(excel_writer=writer,sheet_name='sheet1')
     writer.save()
@@ -134,73 +123,11 @@ def get_X_Equivalent_Disp_info(filename,pattern_mode):
     g.close()
     return filename
 
-def get_Y_Equivalent_Disp_info(filename,pattern_mode):
-    Y_E_Dispangle = []
-    floor=[]
-    info = get_block_info(pattern_mode)
-    g = open("%s.txt"%filename,"w",encoding="utf-8")
-    g.write(info[0])
-    g = open("%s.txt"%filename,"r",encoding="utf-8")
-    lines = g.readlines()
-    # print(lines)
-    # print("&&&&&&"*5)
-    lines = lines[2:]
-    lines = lines[:-2]
-    del lines[2]
-    for i in range(len(lines)):
-        tt=lines[i]
-        if i >1:
-            if  i%2==1:
-                stringtemp =tt[49:55]
-                # print(stringtemp)
-                # nostringtemp = ''.join(stringtemp.split())
-                # print(stringtemp)
-                nostringtemp=convert_to_float(stringtemp)
-                Y_E_Dispangle.append(nostringtemp)
-            else:
-                stringtemp1 =tt[0:5]
-                nostringtemp = ''.join(stringtemp1.split())
-                nostringtemp=convert_to_float(nostringtemp)
-                floor.append(nostringtemp)       
-
-    # print("**********")
-    # print(floor)
-    # print("**********")      
-    # print(X_E_Dispangle)       
-    # print("**********")
-    pd1=pd.DataFrame(floor,columns=['楼层'])
-    pd2=pd.DataFrame(Y_E_Dispangle,columns=['位移角'])
-    # pd.to_numeric(pd1, errors='coerce')
-    # pd.to_numeric(pd2, errors='coerce')
-    pd3=pd.concat([pd1,pd2],axis=1)
-    print(pd3)
-    print('***'*20)
-    print(pd3.dtypes)
-
-    # pd4['B'].plot.barh(stacked=True)
-    # pd4.plot.barh(stacked=True)
-    pd3['位移角'].plot.line(subplots=True)
-    plt.show()
-    time.sleep(1)
-    # pd3.plot.bar(stacked=True)
-
-    filepath='Y方向地震作用下的楼层最大位移.xls'
-    writer = pd.ExcelWriter(filepath)
-    pd3.to_excel(excel_writer=writer,sheet_name='sheet1')
-    writer.save()
-    writer.close()
-
-
-    g = open("%s.txt"%filename,"w",encoding="utf-8")
-    for i in lines:
-        g.write(i+"\n")
-    g.close()
-    return filename
 
 def get_X_Wind_Disp_info(filename,pattern_mode):
     X_W_Dispangle = []
     X_W_Dispangle_f = []
-    floor=[]
+    floor=[] 
     info = get_block_info(pattern_mode)
     # print(info[0])
     g = open("%s.txt"%filename,"w",encoding="utf-8")
@@ -213,7 +140,6 @@ def get_X_Wind_Disp_info(filename,pattern_mode):
     lines = lines[5:]
     lines = lines[:-2]
     # print(lines)
-
     # del lines[2]
     for i in range(len(lines)):
         tt=lines[i]
@@ -232,20 +158,14 @@ def get_X_Wind_Disp_info(filename,pattern_mode):
     pd1=pd.DataFrame(floor,columns=['楼层'])
     pd2=pd.DataFrame(X_W_Dispangle,columns=['位移角'])
     pd21=pd.DataFrame(X_W_Dispangle_f,columns=['分数位移角'])
-    # pd.to_numeric(pd1, errors='coerce')
-    # pd.to_numeric(pd2, errors='coerce')
     pd3=pd.concat([pd1,pd2,pd21],axis=1)
     print(pd3)
     print('***'*20)
     print(pd3.dtypes)
-
-    # pd4['B'].plot.barh(stacked=True)
-    # pd4.plot.barh(stacked=True)
     pd3['位移角'].plot.line(subplots=True)
     plt.show()
     time.sleep(1)
-    # pd3.plot.bar(stacked=True)
-
+ 
     filepath="%s.xls" %filename
     writer = pd.ExcelWriter(filepath)
     pd3.to_excel(excel_writer=writer,sheet_name='sheet1')
@@ -259,37 +179,10 @@ def get_X_Wind_Disp_info(filename,pattern_mode):
     return filename
 
 if __name__ == '__main__':
-    # data_into_xls("X方向地震作用下的楼层最大位移",get_X_Equivalent_Disp_info("disp_1","X 方向地震作用下的楼层最大位移(.*?)X向最大层间位移角"))
-    # data_into_xls("Y方向地震作用下的楼层最大位移",get_Y_Equivalent_Disp_info("disp_2","Y 方向地震作用下的楼层最大位移(.*?)Y向最大层间位移角"))
+    data_into_xls("X方向地震作用下的楼层最大位移",get_X_Equivalent_Disp_info("disp_1","X 方向地震作用下的楼层最大位移(.*?)X向最大层间位移角"))
+    data_into_xls("Y方向地震作用下的楼层最大位移",get_X_Equivalent_Disp_info("disp_2","Y 方向地震作用下的楼层最大位移(.*?)Y向最大层间位移角"))
     data_into_xls("+X方向风荷载作用下的楼层最大位移",get_X_Wind_Disp_info("disp_31","(\+)X 方向风荷载作用下的楼层最大位移(.*?)X向最大层间位移角"))
-    # data_into_xls("-Y方向风荷载作用下的楼层最大位移",get_X_Wind_Disp_info("disp_32","(\-)X 方向风荷载作用下的楼层最大位移(.*?)X向最大层间位移角"))
-
-    # data_into_xls("各层构件数量、构件材料和层高",get_num_compon_mater_heig_info("new_2","各层构件数量、构件材料和层高(.*?)保护层"))
-    # data_into_xls("混凝土构件",get_concrte_info("new_3","混凝土构件：(.*?)箍筋（墙分布筋）"))
-    # # data_into_xls("型钢混凝土构件",get_Shaped_steel_concrete_info("new_4","型钢混凝土构件：(.*?)箍筋"))
-    # data_into_xls("箍筋（墙分布筋）",get_Distribution_strirrup_of_wall_reinforcement_info("new_5","箍筋（墙分布筋）：(.*?) X、Y方向剪力墙截面面积"))
-    # data_into_xls("X、Y方向剪力墙截面面积",get_Shear_wall_section_area_info("new_6","X、Y方向剪力墙截面面积(.*?)风荷载信息"))
-    # data_into_xls("风荷载信息",get_wind_load_info("new_7","风荷载信息\n(.*?)各楼层等效尺寸"))
-    # data_into_xls("各楼层等效尺寸",get_Equivalent_dimensions_of_each_floor_info("new_8","各楼层等效尺寸(.*?)各楼层质量、单位面积质量分"))
-    # data_into_xls("各楼层质量、单位面积质量分布",get_Unit_area_mass_distribution_info("new_9","各楼层质量、单位面积质量分布(.*?)计算时间"))
-    # # data_into_xls("楼层抗剪承载力验算",get_Floor_shear_bearing_capacity_info("new_10","表示本层与上一层的承载力之比(.*?)+$"))
-    # data_into_xls("风荷载作用下剪力平衡验算",get_Calculation_hear_balance_under_wind_load_info("new_11","风荷载作用下剪力平衡验算(.*?)楼层抗剪承载力验算"))
-    # data_into_xls("恒、活荷载作用下轴力平衡验算",get_Check_the_axial_force_balance_info("new_12","恒、活荷载作用下轴力平衡验算(.*?)风荷载作用下剪力平衡验算"))
-    # data_into_xls("结构抗震验算",get_Structural_seismic_checking_info("new_13","结构抗震验算(.*?)风振舒适度验算"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    data_into_xls("-Y方向风荷载作用下的楼层最大位移",get_X_Wind_Disp_info("disp_32","(\-)X 方向风荷载作用下的楼层最大位移(.*?)X向最大层间位移角"))
+    data_into_xls("+Y方向风荷载作用下的楼层最大位移",get_X_Wind_Disp_info("disp_33","(\+)Y 方向风荷载作用下的楼层最大位移(.*?)Y向最大层间位移角"))
+    data_into_xls("-Y方向风荷载作用下的楼层最大位移",get_X_Wind_Disp_info("disp_34","(\-)Y 方向风荷载作用下的楼层最大位移(.*?)Y向最大层间位移角"))   
 
